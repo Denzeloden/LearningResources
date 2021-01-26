745ffb58457bef2009ab3fe99b9a4978f900f4aa
Denzel Oden <60088222+Denzeloden@users.noreply.github.com>
2021-01-26 15:30:37 -0500

    Add files via upload
    C# program to reverse a input string

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS412_OdenW2LabReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line = "";
+            string name = "";
+            line = Console.ReadLine();
+            Console.WriteLine("What is your name? " + line);
+            Console.ReadKey();
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                name = name + line[i];
+            }
+
+            Console.WriteLine("Your name in reverse is: " + name);
+            Console.ReadLine();
+        }
+    }
+}
